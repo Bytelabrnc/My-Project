8daad60009134165cb67fe7f55c9cd58942b7d0a
Byte Lab <sujit.singh358@gmail.com>
2024-12-12 10:39:40 +0000

    t

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,7 +3,7 @@ let accountemail = "sujit@gmail.com"
 var accountpass = "12345"
 accountcity = "Ranchi"
 
-// account_id = 2 // not Allowed
+// accountid = 2 // not Allowed
 
 // console.log(accountid);
 /* 
