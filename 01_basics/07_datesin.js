488d7bd093ef673c80b349e59a33f20d9dd46f26
Byte Lab <sujit.singh358@gmail.com>
2024-12-14 11:01:53 +0000

    Dates

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -1,8 +1,10 @@
 // Dates
  
-let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
