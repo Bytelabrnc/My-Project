bade1285f16185721df7140a377b92c0e571c33b
Byte Lab <sujit.singh358@gmail.com>
2024-12-14 10:00:16 +0000

    string done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,7 +4,8 @@ const repoCount = 50
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('Byte-lab-ranchi')
-
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.toLowerCase());
@@ -13,21 +14,21 @@ console.log(gameName.charAt(10));
 
 console.log(gameName.indexOf('i'));
 
-// const newString = gameName.substring(0,7)
-// console.log(newString);
+const newString = gameName.substring(0,7)
+console.log(newString);
 
-const anotherString = gameName.slice(-2, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-2, 4)
+// console.log(anotherString);
 
 
-const newStringOne = "  Sujit "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "  Sujit "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://bytelab.in/services%20home"
-console.log(url.replace('%20', '-'));
+// const url = "https://bytelab.in/services%20home"
+// console.log(url.replace('%20', '-'));
 
-console.log(url.includes('servicess'));
+// console.log(url.includes('servicess'));
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
 
