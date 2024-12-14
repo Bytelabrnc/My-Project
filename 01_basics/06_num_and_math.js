49a708420f61052482d313f6f0b65f6d811badbc
Byte Lab <sujit.singh358@gmail.com>
2024-12-14 10:46:29 +0000

    math & num

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -20,16 +20,20 @@ const hunderds = 100000
 
 console.log(Math);
 // console.log(Math.abs(-4));
-// console.log(Math.round(4, 6));
-// console.log(Math.ceil(4.2));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
-// console.log(Maths.min(4,3,6,8));
+// console.log(Math.min(4,3,6,8));
 
 
 console.log(Math.random()); 
+console.log((Math.random()*10)+ 1);
+console.log(Math.floor(Math.random()*10)+ 1);
 
+const min = 10
+const max = 20
 
-
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
 
 
